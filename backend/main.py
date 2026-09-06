4b3e535c5694587c87af4efe6507466c2e0b77bd
Virat Agnihotri <viratagnihotri178@gmail.com>
2026-09-05 18:42:16 -0700

    successfully call image from front to the backend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import router 
 app=FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -8,6 +9,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(router.router)
 
 @app.get("/")
 def home():
--- frontend/src/api/imageApi.js
+++ frontend/src/api/imageApi.js
@@ -1,7 +1,9 @@
+import api from "./axios";
+
 export const processImages =async (image1,image2)=>{
-    const formData =new formData();
-    formData.append(image1);
-    formData.append(image2);
+    const formData =new FormData();
+    formData.append("image1",image1);
+    formData.append("image2",image2);
     const response =await api.post("/process-images",formData);
     return response.data;
 }
